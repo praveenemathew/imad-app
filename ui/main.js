9e2259536ac03f635a731db0b187609d8d0e5cfe
praveenemathew <praveen.e.mathew@gmail.com>
2017-09-28 11:45:51 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -14,10 +14,20 @@ console.log('Loaded!');
 //};
 
 var button=document.getElementById("button");
-var counter=0;
 button.onclick=function (){
-  
-  counter=counter+1;
-  var span=document.getElementById("count");
-  span.innerHTML=counter.toString();
+    
+    var request=new XMLHttpRequest();
+    request.onreadystatechnge=function(){
+        
+    if(request.readyState === XMLHttpRequest.DONE){
+        if(request.status===200){
+            var counter= request.responseText();
+            var span=document.getElementById("count");
+            span.innerHTML=counter.toString();
+            
+        }
+     }
+    };
+  request.open('GET','http://praveenemathew.imad.hasura-app.io/counter',true);
+  request.send(null);
 };
