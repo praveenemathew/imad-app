42605d5cc357a5ef81ece384ec6bed1708e64b4c
praveenemathew <praveen.e.mathew@gmail.com>
2017-09-28 08:07:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,8 @@
 console.log('Loaded!');
 var element=document.getElementById("main-text");
 element.innerHTML="New Value";
+
+var image=document.getElementById("madi");
+image.onclick =function() {
+    image.style.marginleft="100px"
+}
