b7254353233a9fed74b33ae9974073ef9942c074
praveenemathew <praveen.e.mathew@gmail.com>
2017-09-28 09:24:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,7 +8,7 @@ function moveRight() {
  marginleft=marginleft+10;
  image.style.marginleft=marginleft+'px';
 }
-image.onClick =function() {
+image.onclick =function() {
    // var interval=setInterval(moveRight,100);
     image.style.marginleft="100px";
 };
