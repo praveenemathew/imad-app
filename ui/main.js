dcbd5097ec0cd81a5bc1b667b2d1d4d3def9e0a6
praveenemathew <praveen.e.mathew@gmail.com>
2017-09-28 11:15:58 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,17 +1,17 @@
 console.log('Loaded!');
-var element=document.getElementById("main-text");
-element.innerHTML="New Value";
+//var element=document.getElementById("main-text");
+//element.innerHTML="New Value";
 
-var image=document.getElementById("madi");
-var marginleft=0;
-function moveRight() {
- marginleft=marginleft+10;
- image.style.marginLeft=marginleft+'px';
-}
-image.onclick =function () {
-   var interval=setInterval(moveRight,100);
+//var image=document.getElementById("madi");
+//var marginleft=0;
+//function moveRight() {
+ //marginleft=marginleft+10;
+ //image.style.marginLeft=marginleft+'px';
+//}
+//image.onclick =function () {
+  // var interval=setInterval(moveRight,100);
     //image.style.marginLeft="600px";
-};
+//};
 
 var button=document.getElementById("button");
 var counter=0;
